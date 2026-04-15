020049e3de968dfbfe2d23c7230030fc8561c766
virghiu <virghiu@gmail.com>
2026-04-15 09:56:28 -0400

    define agents directly

---
+++ pipelines/jenkinsfile-02.groovy
@@ -0,0 +1,15 @@
+pipeline {
+    agent {
+        docker {
+            image 'node:lts-alpine'
+        }
+    }
+
+    stages {
+        stage('Build') {
+            steps {
+                sh 'node -v'
+            }
+        }
+    }
+}
